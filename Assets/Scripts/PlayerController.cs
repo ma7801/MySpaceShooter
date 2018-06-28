4fca43c6b6c545a1c406964d0325555e3bc94b96
Mike <ma7801@gmail.com>
2018-06-28 15:35:01 -0700

    6-28-18  3:30PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,19 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float tilt;
+    public Boundary boundary;
+   
 
     private void FixedUpdate()
     {
+        // Get horizontal and vertical input info from keyboard:
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Rigidbody rb = GetComponent<Rigidbody>();
 
+        // Set the velocity of the object based on the input info
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.velocity = movement * speed;
+
+        // Ensure that the position does not exceed the boundaries
+        rb.position = new Vector3(
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+         );
+        
+        // Tilt the ship based on the ships x-velocity
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
     }
 
+    
 }
