d2b74bbea09eebd3612d4a3945b6fff9ef72edf3
ma7801 <ma7801@gmail.com>
2018-07-05 22:24:47 -0700

    7-5-18 Custom Modifications in Hazards

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,37 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+[System.Serializable]
+public class Hazards
+{
+    public GameObject[] asteroids;
+
+
+}
+
 public class GameController : MonoBehaviour {
 
-    public GameObject hazard;
+    public GameObject[] hazards;
+    public float hazardSpeedIncreaseFactor;
+    //public Hazards hazards;
     public Vector3 spawnValue;
     public int hazardCount;
+    public int hazardCountIncrementPerWave;
     public float spawnWait;
     public float startWait;
     public float waveWait;
     public TextMeshPro scoreText;
     private int score;
+    private bool isFirstHazardInstance;
+    private float curHazardSpeed;
 
     void Start()
     {
+        isFirstHazardInstance = true;
         score = 0;
-        updateScore();
+        UpdateScore();
         StartCoroutine (SpawnWaves());
     }
 
     IEnumerator SpawnWaves()
     {
         yield return new WaitForSeconds(startWait);
+
+        GameObject curHazard = new GameObject();
+        
         while (true)
         {
             for (int i = 0; i < hazardCount; i++)
             {
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
                 Quaternion spawnRotation = Quaternion.identity;
-                Instantiate(hazard, spawnPosition, spawnRotation);
+
+                // Instantiate the hazard and set its speed
+                curHazard = Instantiate(hazards[Random.Range(0, hazards.Length)], spawnPosition, spawnRotation);
+
+                // Get current hazard speed (for calculating faster speeds for future waves)
+                if (isFirstHazardInstance)
+                {
+                    curHazardSpeed = curHazard.GetComponent<Mover>().speed;
+                    isFirstHazardInstance = false;
+                }
+                else
+                {
+                    curHazard.GetComponent<Mover>().speed = curHazardSpeed;
+                }
                 yield return new WaitForSeconds(spawnWait);
             }
 
+            hazardCount += hazardCountIncrementPerWave;
+            curHazardSpeed *= hazardSpeedIncreaseFactor;
             yield return new WaitForSeconds(waveWait);
         }
     }
@@ -41,10 +73,10 @@ public class GameController : MonoBehaviour {
     public void AddScore (int scoreIncrement)
     {
         score += scoreIncrement;
-        updateScore();
+        UpdateScore();
     }
 
-    void updateScore()
+    void UpdateScore()
     {
         scoreText.text = "Score: " + score;
     }
